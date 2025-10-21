95381a0e4232eb59e4eccf4c5571202e62120028
pedrohsouza <pedrosouza185@gmail.com>
2025-10-21 19:56:46 -0300

    condicionais, aritmética, interpolação

--- python/teste_aula1.py
+++ python/teste_aula1.py
@@ -1,4 +1,18 @@
-print("-----------------")
-# print("\n")
-print("Hello, world!\n")
-print("-----------------")
+# print("-----------------")
+# # print("\n") #pula duas linhas
+# # print()  # pula uma linha
+# print("Hello, world!\n")
+# print("-----------------")
+
+a = 5
+b = 5
+c = a + b
+print("Soma = ", c)
+
+c = a - b
+print(f"Subtração = {c}")
+
+if c >= 6:
+    print("c eh maior ou igual a 6")
+else:
+    print("c eh menor do que 6")
