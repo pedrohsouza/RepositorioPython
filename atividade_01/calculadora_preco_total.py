9d85ada037f3c27d7926f6690edee3bd7aef3cb5
pedrohsouza <pedrosouza185@gmail.com>
2025-10-27 03:09:19 -0300

    Adiciona atividade prática 01

---
+++ atividade_01/calculadora_preco_total.py
@@ -0,0 +1,11 @@
+nome_do_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Informações da compra:")
+print(f"Nome do produto: {nome_do_produto}")
+print(f"Preço unitário: R$ {preco_unitario}")
+print(f"Quantidade: {quantidade}")
+print(f"Total: R$ {preco_total}")
