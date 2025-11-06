362ef55a40c6296ba19ea874b1b3d055cfceaaa5
pedrohsouza <pedrosouza185@gmail.com>
2025-11-06 01:47:57 -0300

    Corrige print desconto

--- atividade_02/calculdadora_de_desconto.py
+++ atividade_02/calculdadora_de_desconto.py
@@ -6,5 +6,5 @@ preco_final = preco_original - (preco_original * porcentagem_desconto) / 100
 
 print(f"Nome do produto: {nome_do_produto}")
 print(f"Preço original: R$ {preco_original}")
-print(f"Porcentagem de desconto: R$ {preco_original}")
+print(f"Porcentagem de desconto: {porcentagem_desconto}%")
 print(f"O preço final com desconto de {porcentagem_desconto}% é: R$ {preco_final:.2f}")
